fd9c47221c31b640451355b7e40e196745386950
kantuni <henrikh.kantuni@gmail.com>
2018-09-29 06:31:20 +0400

    1083: Factorials

---
+++ 1083/factorials.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n, int k) {
+  if (n == k) {
+    return k;
+  } else if (n < k) {
+    return n % k;
+  }
+  return n * f(n - k, k);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  string exc;
+  cin >> exc;
+  int k = exc.size();
+  cout << f(n, k) << "\n";
+  return 0;
+}
