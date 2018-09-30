f88be3f7d3949d3c1ca3630be62b6b07712954be
kantuni <henrikh.kantuni@gmail.com>
2018-09-30 23:43:04 +0400

    1087: WA

---
+++ 1087/the_time_to_take_stones.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> k(m);
+  for (int i = 0; i < m; i++) {
+    cin >> k[i];
+  }
+  sort(k.rbegin(), k.rend());
+  int turn = 0;
+  while (n > 0) {
+    for (int i = 0; i < m; i++) {
+      if (n - k[i] < 0) {
+        continue;
+      } else {
+        n -= k[i];
+        break;
+      }
+    }
+    turn = !turn;
+  }
+  cout << turn + 1 << "\n";
+  return 0;
+}
